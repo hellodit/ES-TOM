1247c4b88f8e031248dd995c7a8d1bd634535b62
hellodit <asditap@gmail.com>
2019-07-26 15:15:41 +0700

    Add dashborad item

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,33 +1,165 @@
 @extends('layouts.admin.dashboard')
 @section('title','Dashboard')
 @section('main-content')
-<div class="section-header">
-        <h1>Default Layout</h1>
-        <div class="section-header-breadcrumb">
-          <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-          <div class="breadcrumb-item"><a href="#">Layout</a></div>
-          <div class="breadcrumb-item">Default Layout</div>
-        </div>
-      </div>
+<section class="section">
+    <div class="section-body">
+        <section class="section">
+            <div class="section-header">
+                <h1>Dashboard</h1>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-danger">
+                            <i class="far fa-newspaper"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Dana Terkumpul</h4>
+                            </div>
+                            <div class="card-body">
+                               11
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-warning">
+                            <i class="far fa-file"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Donasi Disalurkan</h4>
+                            </div>
+                            <div class="card-body">
+                                11
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-success">
+                            <i class="fas fa-circle"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Campaign Dimulai</h4>
+                            </div>
+                            <div class="card-body">
+                               11
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-danger">
+                            <i class="fas fa-circle"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4>Campaign Dibatalkan</h4>
+                            </div>
+                            <div class="card-body">
+                                11
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-9 col-md-12 col-12 col-sm-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Transaksi terakhir</h4>
+                        </div>
+                        <div class="card-body">
+                            <div id="transactiondata">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-12 col-12 col-sm-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Recent Activities</h4>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-unstyled list-unstyled-border">
+                                <li class="media">
+                                    <img class="mr-3 rounded-circle" width="50" src="../assets/img/avatar/avatar-1.png"
+                                        alt="avatar">
+                                    <div class="media-body">
+                                        <div class="float-right text-primary">Now</div>
+                                        <div class="media-title">Farhan A Mujib</div>
+                                        <span class="text-small text-muted">Cras sit amet nibh libero, in gravida nulla.
+                                            Nulla vel metus scelerisque ante sollicitudin.</span>
+                                    </div>
+                                </li>
+                                <li class="media">
+                                    <img class="mr-3 rounded-circle" width="50" src="../assets/img/avatar/avatar-2.png"
+                                        alt="avatar">
+                                    <div class="media-body">
+                                        <div class="float-right">12m</div>
+                                        <div class="media-title">Ujang Maman</div>
+                                        <span class="text-small text-muted">Cras sit amet nibh libero, in gravida nulla.
+                                            Nulla vel metus scelerisque ante sollicitudin.</span>
+                                    </div>
+                                </li>
+                                <li class="media">
+                                    <img class="mr-3 rounded-circle" width="50" src="../assets/img/avatar/avatar-3.png"
+                                        alt="avatar">
+                                    <div class="media-body">
+                                        <div class="float-right">17m</div>
+                                        <div class="media-title">Rizal Fakhri</div>
+                                        <span class="text-small text-muted">Cras sit amet nibh libero, in gravida nulla.
+                                            Nulla vel metus scelerisque ante sollicitudin.</span>
+                                    </div>
+                                </li>
+                                <li class="media">
+                                    <img class="mr-3 rounded-circle" width="50" src="../assets/img/avatar/avatar-4.png"
+                                        alt="avatar">
+                                    <div class="media-body">
+                                        <div class="float-right">21m</div>
+                                        <div class="media-title">Alfa Zulkarnain</div>
+                                        <span class="text-small text-muted">Cras sit amet nibh libero, in gravida nulla.
+                                            Nulla vel metus scelerisque ante sollicitudin.</span>
+                                    </div>
+                                </li>
+                            </ul>
+                            <div class="text-center pt-1 pb-1">
+                                <a href="#" class="btn btn-primary btn-lg btn-round">
+                                    View All
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</section>
+@endsection
+@section('customjs')
+<script>
+    $(document).ready(function(){
+        $("#transactiondata").load('{{url("dashboard/transaction/data")}}');
 
-      <div class="section-body">
-        <h2 class="section-title">This is Example Page</h2>
-        <p class="section-lead">This page is just an example for you to create your own page.</p>
-        <div class="card">
-          <div class="card-header">
-            <h4>Example Card</h4>
-          </div>
-          <div class="card-body">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-          <div class="card-footer bg-whitesmoke">
-            This is card footer
-          </div>
-        </div>
-      </div>
+        $(document).on('click','.pagination a',function(event){
+            event.preventDefault();
+            var page = $(this).attr('href').split('page=')[1];
+            fetch_data(page)
+        })
+
+        function fetch_data(page){
+            $.ajax({
+                url: "{{url('dashboard/transaction/data?page=')}}"+page,
+                success:function(data){
+                    $("#transactiondata").html(data);
+                }
+            })
+        }
+    });
+</script>
 @endsection
