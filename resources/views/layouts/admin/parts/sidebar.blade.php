9b6a9de7ec1c2687e6a63fecae7e253767ac6a88
hellodit <asditap@gmail.com>
2019-08-30 13:31:45 +0700

    Edit sidebar menu

--- resources/views/layouts/admin/parts/sidebar.blade.php
+++ resources/views/layouts/admin/parts/sidebar.blade.php
@@ -8,17 +8,24 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Pages</li>
-            <li><a class="nav-link" href="{{url('dashboard')}}"><i class="fas fa-pencil-ruler"></i> <span>Dashboard</span></a>
-            <li><a class="nav-link" href="{{url('dashboard/campaign')}}"><i class="fas fa-pencil-ruler"></i> <span>Post Campaign</span></a>
+            <li><a class="nav-link" href="{{url('dashboard')}}"><i class="fas fa-pencil-ruler"></i>
+                    <span>Dashboard</span></a>
             <li class="nav-item dropdown">
-                    <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i> <span>Pengaturan</span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="{{route('dashboard.setting')}}">Pengaturan Akun</a></li>
-                    </ul>
-                </li>
-
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i> <span>Master Data</span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="{{route('dashboard.games')}}">Master Permainan</a></li>
+                    <li><a href="#">Master Perilaku Anak</a></li>
+                    <li><a href="#">Master Aturan</a></li>
+                </ul>
+            </li>
+            </li>
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i> <span>Pengaturan</span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="{{route('dashboard.setting')}}">Pengaturan Akun</a></li>
+                </ul>
+            </li>
             </li>
         </ul>
     </aside>
 </div>
-
