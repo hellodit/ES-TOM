91e1845db1c59360526170bfa82a938e9f03119b
hellodit <asditap@gmail.com>
2019-08-30 13:33:56 +0700

    Add middleware in dashboard

--- app/Http/Controllers/Dashboard/HomeDashboardController.php
+++ app/Http/Controllers/Dashboard/HomeDashboardController.php
@@ -7,6 +7,12 @@ use App\Http\Controllers\Controller;
 
 class HomeDashboardController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+
+    }
+
     public function index(){
         return view('dashboard.index');
     }
