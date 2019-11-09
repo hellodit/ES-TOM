1b0467d777a6e5fdbe03d6e808065b983b9799d5
hellodit <asditap@gmail.com>
2019-11-09 15:20:16 +0700

    Add gates to menu view

--- resources/views/layouts/admin/parts/sidebar.blade.php
+++ resources/views/layouts/admin/parts/sidebar.blade.php
@@ -10,30 +10,29 @@
             <li class="menu-header">Halaman</li>
             <li><a class="nav-link" href="{{url('dashboard')}}"><i class="fas fa-pencil-ruler"></i>
                     <span>Dashboard</span></a></li>
-            <li><a class="nav-link" href="{{route('dashboard.users')}}"><i class="fas fa-user"></i>
-                    <span>Daftar Pengguna</span></a></li>
+
             <li><a class="nav-link" href="{{route('dashboard.consul')}}"><i class="fas fa-history"></i>
                         <span>Konsultasi</span></a></li>
             <li><a class="nav-link" href="{{route('dashboard.history')}}"><i class="fas fa-history"></i>
                     <span>Riwayat Konsultasi</span></a></li>
 
             <li class="menu-header">Sistem</li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i> <span>Manajemen Data</span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="{{route('dashboard.games')}}">Manajemen Permainan</a></li>
-                    <li><a href="{{route('dashboard.parameters')}}">Manajemen Parameter</a></li>
-                    {{-- <li><a href="{{route('dashboard.gameparam')}}">Basis Pengetahuan</a></li> --}}
-                    <li><a href="{{route('dashboard.rules')}}">Manajemen Aturan</a></li>
-                </ul>
-            </li>
 
+            @can('admin-only', Auth::User())
+            <li><a class="nav-link" href="{{route('dashboard.users')}}"><i class="fas fa-user"></i>
+                <span>Daftar Pengguna</span></a></li>
             <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i> <span>Pengaturan</span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="{{route('dashboard.setting')}}">Pengaturan Akun</a></li>
-                </ul>
-            </li>
+                    <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i> <span>Manajemen Data</span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="{{route('dashboard.games')}}">Manajemen Permainan</a></li>
+                        <li><a href="{{route('dashboard.parameters')}}">Manajemen Parameter</a></li>
+                        {{-- <li><a href="{{route('dashboard.gameparam')}}">Basis Pengetahuan</a></li> --}}
+                        <li><a href="{{route('dashboard.rules')}}">Manajemen Aturan</a></li>
+                    </ul>
+                </li>
+            @endcan
+            <li><a class="nav-link" href="{{route('dashboard.setting')}}"><i class="fas fa-user"></i>
+                <span>Pengaturan Akun</span></a></li>
             </li>
         </ul>
     </aside>
