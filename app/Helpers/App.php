b7df74448a9ee3c2d4d41f6342b4871f39f48525
hellodit <asditap@gmail.com>
2019-11-12 10:11:49 +0700

    Add helper calculate age

--- app/Helpers/App.php
+++ app/Helpers/App.php
@@ -7,3 +7,15 @@ if (!function_exists('consts')){
     };
 
 }
+
+if(!function_exists('hitungusia')){
+    function hitungusia($tanggal_lahir) {
+        list($year,$month,$day) = explode("-",$tanggal_lahir);
+        $year_diff  = date("Y") - $year;
+        $month_diff = date("m") - $month;
+        $day_diff   = date("d") - $day;
+        if ($month_diff < 0) $year_diff--;
+            elseif (($month_diff==0) && ($day_diff < 0)) $year_diff--;
+        return $year_diff.' Tahun '.$month_diff.' Bulan '.$day_diff.' Hari';
+    }
+}
