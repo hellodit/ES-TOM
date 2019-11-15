2090dba5161465c4b1a3720564bf3a951fd24675
hellodit <asditap@gmail.com>
2019-11-15 09:24:33 +0700

    Restructure html

--- resources/views/dashboard/history/detail.blade.php
+++ resources/views/dashboard/history/detail.blade.php
@@ -28,10 +28,10 @@
                                     <td>{{$history->childname}}</td>
                                 </tr>
                                 <tr>
-                                        <td>Jenis Kelamin</td>
-                                            <td>{{$history->gender == 'L' ? 'Laki-laki' : 'Perempuan'}}</td>
-                                        </tr>
-                                    </tr>
+                                    <td>Jenis Kelamin</td>
+                                    <td>{{$history->gender == 'L' ? 'Laki-laki' : 'Perempuan'}}</td>
+                                </tr>
+                                </tr>
                                 <tr>
                                     <td>Tannggal Lahir</td>
                                     <td>{{$history->birthday}}</td>
@@ -41,8 +41,8 @@
                                     <td>{{hitungusia($history->birthday)}}</td>
                                 </tr>
 
-                                    <td>Tanggal Konsultasi</td>
-                                    <td>{{indo_date($history->created_at,true)}}
+                                <td>Tanggal Konsultasi</td>
+                                <td>{{indo_date($history->created_at,true)}}
                                 </td>
                                 </tr>
                                 <tr>
@@ -60,63 +60,67 @@
                         <h4>Hasil Permainan</h4>
                     </div>
                     <div class="card-body">
-                            @if (!empty($history->rule))
-                                <h3>{{$history->rule->game->name}}</h3>
-                                <div class="row">
-                                    <div class="col-md-3">
-                                        <img src="{{asset("uploads/images/".$history->rule->game->image)}}" class="img-thumbnail" alt="">
-                                    </div>
-                                    <div class="col-md-9">
-                                            {!!$history->rule->game->description!!}
-                                            <p class="mb-2"><span class="badge badge-primary">Kode Permainan : {{$history->rule->game->code}}</span> <span class="badge badge-primary">Kategori : {{$history->rule->game->category}}</span> </p>
-                                    </div>
-                                </div>
-                            @else
-                                <div class="empty-state" data-height="400" style="height: 400px;">
-                                    <div class="empty-state-icon bg-danger">
-                                        <i class="fas fa-times"></i>
-                                    </div>
-                                    <h2>Mohon Maaf :(</h2>
-                                    <p class="lead">
-                                        Untuk saat ini tidak ditemukan jenis permainan edukasi yang sesuai dengan kriteria anak anda.
-                                    </p>
-                                    <a href="{{route('dashboard.consul')}}" class="btn btn-warning mt-4">Coba Lagi</a>
+                        @if (!empty($history->rule))
+                        <h3>{{$history->rule->game->name}}</h3>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img src="{{asset("uploads/images/".$history->rule->game->image)}}"
+                                    class="img-thumbnail" alt="">
                             </div>
-                            @endif
+                            <div class="col-md-9">
+                                {!!$history->rule->game->description!!}
+                                <p class="mb-2"><span class="badge badge-primary">Kode Permainan :
+                                        {{$history->rule->game->code}}</span> <span class="badge badge-primary">Kategori
+                                        : {{$history->rule->game->category}}</span> </p>
+                            </div>
+                        </div>
+                        @else
+                        <div class="empty-state" data-height="400" style="height: 400px;">
+                            <div class="empty-state-icon bg-danger">
+                                <i class="fas fa-times"></i>
+                            </div>
+                            <h2>Mohon Maaf :(</h2>
+                            <p class="lead">
+                                Untuk saat ini tidak ditemukan jenis permainan edukasi yang sesuai dengan kriteria anak
+                                anda. kami akan terus melakukan pembaharuan untuk memberikan lebih banyak pilihan.
+                            </p>
+                            <a href="{{route('dashboard.consul')}}" class="btn btn-warning mt-4">Coba Lagi</a>
+                        </div>
+                        @endif
                     </div>
                 </div>
             </div>
         </div>
         @if (!empty($history->rule))
         <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                                <h4>Pengisian Konsultasi</h4>
-                        </div>
-                        {{-- {{dd($selected)}} --}}
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-md">
-                                    <thead>
-                                            <tr>
-                                                <td>Variabel</td>
-                                                <td>Parameter</td>
-                                            </tr>
-                                    </thead>
-                                    @foreach ($history->rule->params as $key => $item)
-                                        <tr>
-                                            <td>{{$key+1}}</td>
-                                            <td>{{strip_tags($item->name)}}</td>
-                                        </tr>
-                                        @endforeach
-                                </table>
-                            </div>
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Pengisian Konsultasi</h4>
+                    </div>
+                    {{-- {{dd($selected)}} --}}
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-md">
+                                <thead>
+                                    <tr>
+                                        <td>Variabel</td>
+                                        <td>Parameter</td>
+                                    </tr>
+                                </thead>
+                                @foreach ($history->rule->params as $key => $item)
+                                <tr>
+                                    <td>{{$key+1}}</td>
+                                    <td>{{strip_tags($item->name)}}</td>
+                                </tr>
+                                @endforeach
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
-            @endif
+        </div>
+        @endif
 
     </div>
 </div>
--- resources/views/dashboard/master-game/detail.blade.php
+++ resources/views/dashboard/master-game/detail.blade.php
@@ -4,7 +4,9 @@
     </div>
     <div class="col-9">
         <h2>{{$data->name}}</h2>
-        <p class="mb-2"><span class="badge badge-primary">Kode Permainan : {{$data->code}}</span>  | <span class="badge badge-primary">Kategori : {{$data->category}}</span> | <i class="fa fa-clock"></i> {{$data->created_at->diffForHumans()}} </p>
+        <p class="mb-2"><span class="badge badge-primary">Kode Permainan : {{$data->code}}</span> | <span
+                class="badge badge-primary">Kategori : {{$data->category}}</span> | <i class="fa fa-clock"></i>
+            {{$data->created_at->diffForHumans()}} </p>
         {!!$data->description!!}
     </div>
 </div>
